20192113c76b0ca9245d1027fae6b20908361eb1
Connor Gamble <cjgamble21@gmail.com>
2025-07-20 15:41:57 -0500

    Added increment and decrement macros

--- src/CPU/arithmetic.rs
+++ src/CPU/arithmetic.rs
@@ -1,5 +1,38 @@
 use super::CPU;
 use super::utils::*;
+use paste::paste;
+
+macro_rules! inc_8_bit {
+    ($reg:ident) => {
+        paste! {
+            #[inline]
+            pub(crate) fn [<inc_$reg>](&mut self) {
+                self.set_half_carry_add(self.registers.$reg, self.registers.$reg + 1);
+
+                self.registers.$reg += 1;
+
+                self.set_zero_flag(self.registers.$reg);
+                self.set_sub_flag(false);
+            }
+        }
+    };
+}
+
+macro_rules! dec_8_bit {
+    ($reg:ident) => {
+        paste! {
+            #[inline]
+            pub(crate) fn [<dec_$reg>](&mut self) {
+                self.set_half_carry_sub(self.registers.$reg, self.registers.$reg - 1);
+
+                self.registers.$reg -= 1;
+
+                self.set_zero_flag(self.registers.$reg);
+                self.set_sub_flag(true);
+            }
+        }
+    };
+}
 
 impl CPU {
     pub(crate) fn inc_bc(&mut self) {
@@ -8,22 +41,23 @@ impl CPU {
         self.registers.set_bc(data);
     }
 
-    pub(crate) fn inc_b(&mut self) {
-        self.set_half_carry_add(self.registers.b, self.registers.b + 1);
+    // Single register increments
+    inc_8_bit!(a);
+    inc_8_bit!(b);
+    inc_8_bit!(c);
+    inc_8_bit!(d);
+    inc_8_bit!(e);
+    inc_8_bit!(h);
+    inc_8_bit!(l);
 
-        self.registers.b += 1;
-
-        self.set_zero_flag(self.registers.b);
-        self.set_sub_flag(false);
-    }
-
-    pub(crate) fn dec_b(&mut self) {
-        self.set_half_carry_sub(self.registers.b, self.registers.b - 1);
-        self.registers.b -= 1;
-
-        self.set_zero_flag(self.registers.b);
-        self.set_sub_flag(true);
-    }
+    // Single register decrements
+    dec_8_bit!(a);
+    dec_8_bit!(b);
+    dec_8_bit!(c);
+    dec_8_bit!(d);
+    dec_8_bit!(e);
+    dec_8_bit!(h);
+    dec_8_bit!(l);
 
     pub(crate) fn add_hl_bc(&mut self) {
         let half_carry = half_carry_occurred_16(self.registers.bc(), self.registers.hl());
@@ -38,22 +72,5 @@ impl CPU {
 
     pub(crate) fn dec_bc(&mut self) {
         self.registers.set_bc(self.registers.bc() - 1);
-    }
-
-    pub(crate) fn inc_c(&mut self) {
-        self.set_half_carry_add(self.registers.c, self.registers.c + 1);
-        self.registers.c += 1;
-
-        self.set_zero_flag(self.registers.c);
-        self.set_sub_flag(false);
-    }
-
-    pub(crate) fn dec_c(&mut self) {
-        self.set_half_carry_sub(self.registers.b, self.registers.b - 1);
-
-        self.registers.b -= 1;
-
-        self.set_zero_flag(self.registers.b);
-        self.set_sub_flag(true);
     }
 }
