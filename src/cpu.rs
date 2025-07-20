ac7c5f166c938b56a740f28afbf05fa611ad11a7
Connor Gamble <cjgamble21@gmail.com>
2025-07-20 14:57:53 -0500

    More opcodes and reorganization

--- src/cpu.rs
+++ src/cpu.rs
@@ -22,7 +22,7 @@ fn carry_occurred_16(a: u16, b: u16) -> bool {
 
 const BUS_SIZE: usize = 65536; // 64 KB
 
-const INSTRUCTIONS: [Instruction; 11] = [
+const INSTRUCTIONS: [Instruction; 15] = [
     Instruction::new("NOP", CPU::nop, 1),
     Instruction::new("LD_BC_NN", CPU::ld_bc_nn, 3),
     Instruction::new("LD_MEM_BC_A", CPU::ld_mem_bc_a, 2),
@@ -30,10 +30,14 @@ const INSTRUCTIONS: [Instruction; 11] = [
     Instruction::new("INC_B", CPU::inc_b, 1),
     Instruction::new("DEC_B", CPU::dec_b, 1),
     Instruction::new("LD_IMM_B", CPU::ld_imm_b, 2),
-    Instruction::new("RLC_A", CPU::rlc_a, 1),
+    Instruction::new("ROTATE_LEFT_A", CPU::rotate_left_a, 1),
     Instruction::new("LD_MEM_SP", CPU::ld_mem_sp, 5),
     Instruction::new("ADD_HL_BC", CPU::add_hl_bc, 2),
     Instruction::new("LD_MEM_A_BC", CPU::ld_mem_a_bc, 2),
+    Instruction::new("DEC_BC", CPU::dec_bc, 2),
+    Instruction::new("INC_C", CPU::inc_c, 1),
+    Instruction::new("LD_IMM_C", CPU::ld_imm_c, 2),
+    Instruction::new("ROTATE_RIGHT_A", CPU::rotate_right_a, 1),
 ];
 
 pub struct CPU {
@@ -86,7 +90,7 @@ impl CPU {
         self.registers.f.half_carry = a & 0xf < b & 0xf;
     }
 
-    fn nop(&mut self) {}
+    /* #region Memory */
 
     fn ld_bc_nn(&mut self) {
         let low_byte = self.read(self.registers.pc);
@@ -105,6 +109,45 @@ impl CPU {
 
         self.write(addr, self.registers.a);
     }
+
+    fn ld_imm_b(&mut self) {
+        let data = self.read(self.registers.pc);
+        self.registers.pc += 1;
+        self.registers.b = data;
+    }
+
+    fn ld_mem_sp(&mut self) {
+        let low_byte = self.read(self.registers.pc);
+        self.registers.pc += 1;
+
+        let high_byte = self.read(self.registers.pc);
+        self.registers.pc += 1;
+
+        let addr = ((high_byte as u16) << 8) | (low_byte as u16);
+
+        let low_byte_sp = (self.registers.sp & 0x00ff) as u8;
+        let high_byte_sp = (self.registers.sp >> 8) as u8;
+
+        self.write(addr, low_byte_sp);
+        self.write(addr + 1, high_byte_sp);
+    }
+
+    fn ld_mem_a_bc(&mut self) {
+        let value = self.read(self.registers.bc());
+
+        self.registers.a = value;
+    }
+
+    fn ld_imm_c(&mut self) {
+        let value = self.read(self.registers.pc);
+        self.registers.pc += 1;
+
+        self.registers.c = value;
+    }
+
+    /* #endregion */
+
+    /* #region Arithmetic */
 
     fn inc_bc(&mut self) {
         let mut data = self.registers.bc();
@@ -129,35 +172,6 @@ impl CPU {
         self.set_sub_flag(true);
     }
 
-    fn ld_imm_b(&mut self) {
-        let data = self.read(self.registers.pc);
-        self.registers.pc += 1;
-        self.registers.b = data;
-    }
-
-    fn rlc_a(&mut self) {
-        let most_significant_bit = (self.registers.a & 0x80) >> 7;
-
-        self.registers.f.carry = most_significant_bit == 1;
-        self.registers.a = (self.registers.a << 1) | most_significant_bit;
-    }
-
-    fn ld_mem_sp(&mut self) {
-        let low_byte = self.read(self.registers.pc);
-        self.registers.pc += 1;
-
-        let high_byte = self.read(self.registers.pc);
-        self.registers.pc += 1;
-
-        let addr = ((high_byte as u16) << 8) | (low_byte as u16);
-
-        let low_byte_sp = (self.registers.sp & 0x00ff) as u8;
-        let high_byte_sp = (self.registers.sp >> 8) as u8;
-
-        self.write(addr, low_byte_sp);
-        self.write(addr + 1, high_byte_sp);
-    }
-
     fn add_hl_bc(&mut self) {
         let half_carry = half_carry_occurred_16(self.registers.bc(), self.registers.hl());
         let carry = carry_occurred_16(self.registers.bc(), self.registers.hl());
@@ -167,12 +181,6 @@ impl CPU {
         self.registers.f.carry = carry;
         self.registers.f.half_carry = half_carry;
         self.registers.f.sub = false;
-    }
-
-    fn ld_mem_a_bc(&mut self) {
-        let value = self.read(self.registers.bc());
-
-        self.registers.a = value;
     }
 
     fn dec_bc(&mut self) {
@@ -186,4 +194,35 @@ impl CPU {
         self.set_zero_flag(self.registers.c);
         self.set_sub_flag(false);
     }
+
+    fn dec_c(&mut self) {
+        self.set_half_carry_sub(self.registers.b, self.registers.b - 1);
+
+        self.registers.b -= 1;
+
+        self.set_zero_flag(self.registers.b);
+        self.set_sub_flag(true);
+    }
+
+    /* #endregion */
+
+    /* #region Bit shifts */
+    fn rotate_left_a(&mut self) {
+        let most_significant_bit = (self.registers.a & 0x80) >> 7;
+
+        self.registers.f.carry = most_significant_bit == 1;
+        self.registers.a = (self.registers.a << 1) | most_significant_bit;
+    }
+
+    fn rotate_right_a(&mut self) {
+        let first_bit = self.registers.a & 1;
+
+        self.registers.f.carry = first_bit == 1;
+
+        self.registers.a = (first_bit << 7) | (self.registers.a >> 1);
+    }
+
+    /* #endregion */
+
+    fn nop(&mut self) {}
 }
