36fae64592cfe4b36d4eda108e397c97fa441d3e
Connor Gamble <cjgamble21@gmail.com>
2025-07-20 21:22:13 -0500

    Added a macro for instructions to support dynamic cycle counts - probably should've accounted for that earlier

--- src/CPU/instructions.rs
+++ src/CPU/instructions.rs
@@ -1,19 +1,32 @@
 use super::CPU;
 
-type InstructionFn = fn(&mut CPU);
+type InstructionFn = fn(&mut CPU) -> u32;
 
 pub struct Instruction {
     name: &'static str,
     pub function: InstructionFn,
-    pub cycles: i32,
 }
 
 impl Instruction {
-    pub const fn new(name: &'static str, function: InstructionFn, cycles: i32) -> Self {
+    pub const fn new(name: &'static str, function: InstructionFn) -> Self {
         Instruction {
             name,
             function,
-            cycles,
         }
     }
+}
+
+#[macro_export]
+macro_rules! instr {
+    // Used with opcodes that have static cycle counts
+    ($name:expr, $func:expr, $cycles:literal) => {
+        Instruction::new($name, |cpu| {
+            $func(cpu);
+            $cycles
+        })
+    };
+    // Allows instruction to return number of cycles - only used for opcodes with dynamic cycle counts
+    ($name:expr, $func:expr) => {
+        Instruction::new($name, $func)
+    };
 }
--- src/CPU/mod.rs
+++ src/CPU/mod.rs
@@ -10,6 +10,7 @@ mod utils;
 use registers::Registers;
 use crate::Memory;
 use instructions::Instruction;
+use crate::instr;
 
 const BUS_SIZE: usize = 65536; // 64 KB
 
@@ -19,38 +20,39 @@ const BUS_SIZE: usize = 65536; // 64 KB
 */
 // Index of each instruction corresponds to its relevant opcode
 const INSTRUCTIONS: &'static [Instruction] = &[
-    Instruction::new("NOP", CPU::nop, 1),
-    Instruction::new("LD_IMM_BC", CPU::ld_imm_bc, 3),
-    Instruction::new("STR_BC_A", CPU::str_ind_bc_a, 2),
-    Instruction::new("INC_BC", CPU::inc_bc, 2),
-    Instruction::new("INC_B", CPU::inc_b, 1),
-    Instruction::new("DEC_B", CPU::dec_b, 1),
-    Instruction::new("LD_IMM_B", CPU::ld_imm_b, 2),
-    Instruction::new("ROTATE_LEFT_A", CPU::rotate_left_a, 1),
-    Instruction::new("STR_SP_MEM", CPU::str_sp_mem, 5),
-    Instruction::new("ADD_HL_BC", CPU::add_hl_bc, 2),
-    Instruction::new("LD_IND_BC_A", CPU::ld_ind_bc_a, 2),
-    Instruction::new("DEC_BC", CPU::dec_bc, 2),
-    Instruction::new("INC_C", CPU::inc_c, 1),
-    Instruction::new("DEC_C", CPU::dec_c, 1),
-    Instruction::new("LD_IMM_C", CPU::ld_imm_c, 2),
-    Instruction::new("ROTATE_RIGHT_A", CPU::rotate_right_a, 1),
-    Instruction::new("STOP", CPU::stop, 1),
-    Instruction::new("LD_IMM_DE", CPU::ld_imm_de, 3),
-    Instruction::new("LD_IND_DE_A", CPU::ld_ind_de_a, 2),
-    Instruction::new("INC_DE", CPU::inc_de, 2),
-    Instruction::new("INC_D", CPU::inc_d, 1),
-    Instruction::new("DEC_D", CPU::dec_d, 1),
-    Instruction::new("LD_IMM_D", CPU::ld_imm_d, 2),
-    Instruction::new("ROTATE_LEFT_A_WITH_CARRY", CPU::rotate_left_a_with_carry, 1),
-    Instruction::new("JUMP_SIGNED_DEFAULT", CPU::jump_signed_default, 3),
-    Instruction::new("ADD_HL_DE", CPU::nop, 2),
-    Instruction::new("LD_IND_DE_A", CPU::ld_ind_de_a, 2),
-    Instruction::new("DEC_DE", CPU::dec_de, 2),
-    Instruction::new("INC_E", CPU::inc_e, 1),
-    Instruction::new("DEC_E", CPU::dec_e, 1),
-    Instruction::new("LD_IMM_E", CPU::ld_imm_e, 2),
-    Instruction::new("ROTATE_RIGHT_A_WITH_CARRY", CPU::rotate_right_a_with_carry, 1),
+    instr!("NOP", CPU::nop, 1),
+    instr!("NOP", CPU::nop, 1),
+    instr!("LD_IMM_BC", CPU::ld_imm_bc, 3),
+    instr!("STR_BC_A", CPU::str_ind_bc_a, 2),
+    instr!("INC_BC", CPU::inc_bc, 2),
+    instr!("INC_B", CPU::inc_b, 1),
+    instr!("DEC_B", CPU::dec_b, 1),
+    instr!("LD_IMM_B", CPU::ld_imm_b, 2),
+    instr!("ROTATE_LEFT_A", CPU::rotate_left_a, 1),
+    instr!("STR_SP_MEM", CPU::str_sp_mem, 5),
+    instr!("ADD_HL_BC", CPU::add_hl_bc, 2),
+    instr!("LD_IND_BC_A", CPU::ld_ind_bc_a, 2),
+    instr!("DEC_BC", CPU::dec_bc, 2),
+    instr!("INC_C", CPU::inc_c, 1),
+    instr!("DEC_C", CPU::dec_c, 1),
+    instr!("LD_IMM_C", CPU::ld_imm_c, 2),
+    instr!("ROTATE_RIGHT_A", CPU::rotate_right_a, 1),
+    instr!("STOP", CPU::stop, 1),
+    instr!("LD_IMM_DE", CPU::ld_imm_de, 3),
+    instr!("LD_IND_DE_A", CPU::ld_ind_de_a, 2),
+    instr!("INC_DE", CPU::inc_de, 2),
+    instr!("INC_D", CPU::inc_d, 1),
+    instr!("DEC_D", CPU::dec_d, 1),
+    instr!("LD_IMM_D", CPU::ld_imm_d, 2),
+    instr!("ROTATE_LEFT_A_WITH_CARRY", CPU::rotate_left_a_with_carry, 1),
+    instr!("JUMP_SIGNED_DEFAULT", CPU::jump_signed_default, 3),
+    instr!("ADD_HL_DE", CPU::nop, 2),
+    instr!("LD_IND_DE_A", CPU::ld_ind_de_a, 2),
+    instr!("DEC_DE", CPU::dec_de, 2),
+    instr!("INC_E", CPU::inc_e, 1),
+    instr!("DEC_E", CPU::dec_e, 1),
+    instr!("LD_IMM_E", CPU::ld_imm_e, 2),
+    instr!("ROTATE_RIGHT_A_WITH_CARRY", CPU::rotate_right_a_with_carry, 1),
 ];
 
 /* 
@@ -62,7 +64,7 @@ const INSTRUCTIONS: &'static [Instruction] = &[
 pub struct CPU {
     memory: Vec<u8>,
     registers: Registers,
-    cycles: i32,
+    cycles: u32,
 }
 
 impl Memory for CPU {
@@ -100,9 +102,9 @@ impl CPU {
     fn execute(&mut self, opcode: u8) {
         let instruction = &INSTRUCTIONS[opcode as usize];
 
-        (instruction.function)(self);
+        let cycles = (instruction.function)(self);
 
-        self.cycles += instruction.cycles;
+        self.cycles += cycles;
     }
 
     fn set_zero_flag(&mut self, result: u8) {
